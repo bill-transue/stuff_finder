6aed1b248ddfc3b1d140fa8116f4b1329d5e13d4
Bill Transue <transue@gmail.com>
2015-09-24 21:00:03 -0400

    add paperclip migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150922235409) do
+ActiveRecord::Schema.define(version: 20150925005737) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -25,8 +25,12 @@ ActiveRecord::Schema.define(version: 20150922235409) do
     t.decimal  "price"
     t.integer  "category_id"
     t.integer  "user_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   add_index "classifieds", ["category_id"], name: "index_classifieds_on_category_id"
